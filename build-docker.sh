23a14392e3e1b3c911ade6bd33a0c86c52d0f920
it <it@blindsailing.ca>
2021-07-25 01:52:08 +0000

    build

--- build-docker.sh
+++ build-docker.sh
@@ -7,23 +7,23 @@ mv ./app ./app_src
 cd ./app_src
 yarn -silent install --frozen-lockfile
 yarn build:app
-# mv ./app ../app
-# cd ../
-# rm -rf ./app_src
-# yarn cache clean
+mv ./app ../app
+cd ../
+rm -rf ./app_src
+yarn cache clean
 
-# # BUILD API
-# echo "=========BUILDING API========="
-# mv ./api ./api_src
-# cd ./api_src
-# yarn -silent install --frozen-lockfile
-# yarn build
-# mv ./dist ./api 
-# cp ./package.json ./api/package.json
-# cp ./yarn.lock ./api/yarn.lock
-# mv ./api ../api
-# cd ../api
-# yarn --prod -silent install --frozen-lockfile
-# cd ../
-# rm -rf ./api_src
-# yarn cache clean
+# BUILD API
+echo "=========BUILDING API========="
+mv ./api ./api_src
+cd ./api_src
+yarn -silent install --frozen-lockfile
+yarn build
+mv ./dist ./api 
+cp ./package.json ./api/package.json
+cp ./yarn.lock ./api/yarn.lock
+mv ./api ../api
+cd ../api
+yarn --prod -silent install --frozen-lockfile
+cd ../
+rm -rf ./api_src
+yarn cache clean
