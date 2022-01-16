7b85bcff19e64c2e3eb3ff7851ea26c7804de80f
bsac it <it@blindsailing.ca>
2022-01-16 03:25:38 +0000

    build app image

--- app/build-docker.sh
+++ app/build-docker.sh
@@ -4,10 +4,12 @@ set -e
 # BUILD APP
 echo "=========BUILDING APP========="
 cd ./app_src
+cp -r ./api ../api
 yarn -silent install --frozen-lockfile
 yarn build
 mv ./start-server.sh ./app/start-server.sh
 mv ./app ../app
 cd ../
 rm -rf ./app_src
+rm -rf ./api
 yarn cache clean
