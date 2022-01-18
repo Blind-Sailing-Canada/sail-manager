0d05f889baacb7d70fbf25b2b240f4205806350f
itbsc <68036154+itbsc@users.noreply.github.com>
2022-01-18 02:12:31 -0500

    Update boat-checklist.entity.ts

--- api/src/boat-checklist/boat-checklist.entity.ts
+++ api/src/boat-checklist/boat-checklist.entity.ts
@@ -10,7 +10,7 @@ import { BoatChecklist } from '../types/boat-checklist/boat-checklist';
 import { BoatChecklistItem } from '../types/boat-checklist/boat-checklist-item';
 import { BoatEntity } from '../boat/boat.entity';
 
-@Entity('boat-checklists')
+@Entity('boat_checklists')
 export class BoatChecklistEntity extends BaseModelEntity implements BoatChecklist {
   @OneToOne(() => BoatEntity, boat => boat.checklist, { eager: false })
   @JoinColumn()
