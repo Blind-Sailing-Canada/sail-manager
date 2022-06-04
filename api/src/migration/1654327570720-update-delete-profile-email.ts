b04ce2e16db06fa62b2191c472ca631c431873a1
alex <it@blindsailing.ca>
2022-06-04 07:40:32 +0000

    update email address of deleted profile

---
+++ api/src/migration/1654327570720-update-delete-profile-email.ts
@@ -0,0 +1,17 @@
+import {
+  MigrationInterface, QueryRunner
+} from 'typeorm';
+
+export class updateDeleteProfileEmail1654327570720 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'UPDATE profiles SET email=CONCAT(email, \':deleted:\', to_char(now()::timestamp at time zone \'UTC\', \'YYYY-MM-DD"T"HH24:MI:SS"Z"\')) WHERE deleted_at IS NOT NULL;'
+    );
+  }
+
+  public async down(): Promise<void> {
+    return Promise.resolve();
+  }
+
+}
