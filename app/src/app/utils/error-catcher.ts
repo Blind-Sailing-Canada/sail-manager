ccbbec9c15e88e512000808b2224d5fa6f34c4b0
it <it@blindsailing.ca>
2024-06-11 05:39:48 +0000

    log error catcher

--- app/src/app/utils/error-catcher.ts
+++ app/src/app/utils/error-catcher.ts
@@ -3,6 +3,7 @@ import { Action } from '@ngrx/store/src/models';
 import { SnackType } from '../models/snack-state.interface';
 import { putSnack } from '../store/actions/snack.actions';
 import { finishLoading } from '../store/actions/app.actions';
+import * as Sentry from '@sentry/browser';
 
 export const errorCatcher = (
   userFriendlyMessage: string,
@@ -11,6 +12,10 @@ export const errorCatcher = (
   notify: boolean = true) => (error) => {
   console.error('Error catcher caught and error: VVV ERROR BELOW VVV');
   console.dir(error);
+
+  Sentry.captureException(error, { extra: {
+    userFriendlyMessage,
+  } });
 
   return of(
     ...(actionsToDispatch || []),
